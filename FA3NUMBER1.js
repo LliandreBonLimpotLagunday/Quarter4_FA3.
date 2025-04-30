47cac4592bec7b49931ac944a81eb652e605d566
LliandreBonLimpotLagunday <lliandrebon.lagunday@smc.pshs.edu.ph>
2025-04-30 20:50:35 +0800

    Create FA3NUMBER1.js

---
+++ FA3NUMBER1.js
@@ -0,0 +1,27 @@
+function getNumberInput() {
+  let input;
+  do {
+    input = prompt("Please enter a number:");
+  } while (isNaN(input) || input.trim() === "");
+
+  return parseInt(input);
+}
+
+function generateShape() {
+  const num = getNumberInput();
+  const outputDiv = document.getElementById("shape-output");
+  let result = `Number inputted: ${num}<br><br>`;
+
+  if (num % 2 === 1) {
+
+    for (let i = num; i >= 1; i--) {
+      result += (i + " ").repeat(num).trim() + "<br>";
+    }
+  } else {
+   
+    for (let i = num; i >= 1; i--) {
+      result += (i + " ").repeat(i).trim() + "<br>";
+    }
+  }
+
+  outputDiv.innerHTML = result;
